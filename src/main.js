15d62cbc4784396a283d70896be061b3704e3e40
Valera Vilks <valeravilks@gmail.com>
2019-08-19 13:40:48 +0300

    Starter template for application

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './style.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDom.render(<div>Start</div>, document.querySelector('#app'));
+
