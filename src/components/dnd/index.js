b07c94900dd33061124aba97e1b3fc3ad3bc9254
Valera Vilks <valeravilks@gmail.com>
2019-08-30 12:54:49 +0300

    Dnd add point

--- src/components/dnd/index.js
+++ src/components/dnd/index.js
@@ -1,32 +1,28 @@
 import withStore from '~/hocs/withStore';
 import React, {Component} from 'react';
 import {SortableContainer, SortableElement} from 'react-sortable-hoc';
-import arrayMove from 'array-move';
 import { ListGroup } from 'react-bootstrap';
 
 const SortableItem = SortableElement(({value}) => <ListGroup.Item>{value}</ListGroup.Item>);
 
-const SortableList = SortableContainer(({items}) => {
+const SortableList = withStore(SortableContainer(({items}) => {
+    console.log(1);
+
     return (
         <ListGroup>
             {items.map((value, index) => (
-                <SortableItem key={`item-${value.name}`} index={index} value={value.name} />
+                <SortableItem key={index} index={index} value={value.name} />
             ))}
         </ListGroup>
     );
-});
+}));
 
 class SortableComponent extends Component {
-    state = {
-        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
-    };
-    onSortEnd = ({oldIndex, newIndex}) => {
-        this.setState(({items}) => ({
-            items: arrayMove(items, oldIndex, newIndex),
-        }));
+     onSortEnd = ({oldIndex, newIndex}) => {
+        this.props.stores.maps.pointerSort(oldIndex, newIndex);
     };
     render() {
-        return <SortableList items={this.props.stores.maps.pointer} onSortEnd={this.onSortEnd} />;
+        return <SortableList key={this.props.stores.maps.pointer.length} items={this.props.stores.maps.pointer} onSortEnd={this.onSortEnd} />;
     }
 }
 
